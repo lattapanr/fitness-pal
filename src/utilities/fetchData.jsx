a78965c6ba507244fb035adfa992a367d5322dd8
Lattapan <noey.rachata@gmail.com>
2023-05-23 22:00:23 +0700

    Add more components/ functionality and styling

--- src/utilities/fetchData.jsx
+++ src/utilities/fetchData.jsx
@@ -1,15 +1,15 @@
 export const exerciseOptions = {
-    method: "GET",
-    url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
-    headers: {
-        "X-RapidAPI-Key": "hidden",
-        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    },
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises",
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
 };
 
 export const fetchData = async (url, options) => {
-    const response = await fetch(url, options);
-    const data = await response.json();
+  const response = await fetch(url, options);
+  const data = await response.json();
 
-    return data;
+  return data;
 };
