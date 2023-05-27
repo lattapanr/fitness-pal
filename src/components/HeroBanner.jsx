d9c7cd83a4b8062666405fd0bed9606f72b38dd6
Lattapan <noey.rachata@gmail.com>
2023-05-27 11:16:36 +0700

    Readjust Home page spacing

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,67 +1,61 @@
-import {
-    Box,
-    Typography,
-    Button,
-    createTheme,
-    ThemeProvider,
-} from "@mui/material";
+import { Box, Typography, createTheme, ThemeProvider } from "@mui/material";
 
+//icon
+import { WiSnowflakeCold } from "react-icons/wi";
+
+//image
 import HeroBannerImg from "../assets/images/banner.png";
 
 const HeroBanner = () => {
-    const theme = createTheme({
-        typography: {
-            fontFamily: ["Gilda Display", "serif"].join(","),
-        },
-    });
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Gilda Display", "serif"].join(","),
+    },
+  });
 
-    return (
-        <Box display="flex" flexDirection="column" mt="90px" sx={{ width: 1 }}>
-            <Box display="flex" justifyContent="space-between">
-                <Box display="flex" flexDirection="column">
-                    <ThemeProvider theme={theme}>
-                        <Typography
-                            color="#000000"
-                            fontWeight="600"
-                            fontSize="65px"
-                        >
-                            Fitness Pal
-                        </Typography>
-                    </ThemeProvider>
-
-                    <Typography
-                        fontWeight={700}
-                        sx={{
-                            fontSize: { lg: "24px", xs: "20px" },
-                            fontStyle: "italic",
-                        }}
-                        mb="50px"
-                        mt="50px"
-                    >
-                        Unleash Your Inner Strength, <br />
-                        Explore the Exercise Spectrum! <br />
-                        Discover a World of Moves for a Fitter You
-                    </Typography>
-                    <button className="explore-button" href="#exercises">
-                        Explore Exercises
-                    </button>
-                </Box>
-                <img
-                    src={HeroBannerImg}
-                    alt="banner"
-                    className="hero-banner-img"
-                />
-            </Box>
-            <Typography
-                fontWeight={600}
-                color="#937a59"
-                sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
-                fontSize="200px"
-            >
-                Exercise
+  return (
+    <Box display="flex" flexDirection="column" mt="50px" sx={{ width: 1 }}>
+      <Box display="flex" justifyContent="space-between">
+        <Box display="flex" flexDirection="column">
+          <ThemeProvider theme={theme}>
+            <Typography color="#000000" fontWeight="600" fontSize="75px">
+              Fitness Pal
             </Typography>
+          </ThemeProvider>
+
+          <Typography
+            fontWeight={700}
+            sx={{
+              fontSize: { lg: "24px", xs: "20px" },
+              fontStyle: "italic",
+            }}
+            mb="50px"
+            mt="50px"
+          >
+            Unleash Your Inner Strength, <br />
+            Explore the Exercise Spectrum! <br />
+            Discover a World of Moves for a Fitter You
+          </Typography>
+          <button className="explore-button" href="#exercises">
+            Explore Exercises
+          </button>
         </Box>
-    );
+        <img src={HeroBannerImg} alt="banner" className="hero-banner-img" />
+      </Box>
+      <Typography
+        fontWeight={600}
+        color="#2c2821"
+        sx={{
+          opacity: "0.1",
+          display: { md: "block", xs: "none" },
+          fontSize: "200px",
+          lineHeight: "250px",
+        }}
+      >
+        Exercise
+      </Typography>
+    </Box>
+  );
 };
 
 export default HeroBanner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,48 +4,43 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
-    return (
-        <header>
-            <Stack
-                sx={{
-                    display: "flex",
-                    alignItems: { sm: "flex-start", xs: "center" },
-                    gap: { sm: "122px", xs: "40px" },
-                    mt: { sm: "150px", xs: "30px" },
-                    justifyContent: "none",
-                }}
-            >
-                <Stack
-                    direction="row"
-                    gap="40px"
-                    fontSize="24px"
-                    alignItems="flex-end"
-                >
-                    <Link
-                        to="/"
-                        style={{
-                            textDecoration: "none",
-                            color: "#000000",
-                            borderBottom: "3px solid #2C2821",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        Home
-                    </Link>
-                    <a
-                        href="#exercises"
-                        style={{
-                            textDecoration: "none",
-                            color: "#000000",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        Exercises
-                    </a>
-                </Stack>
-            </Stack>
-        </header>
-    );
+  return (
+    <header>
+      <Stack
+        sx={{
+          display: "flex",
+          alignItems: { sm: "flex-start", xs: "center" },
+          gap: { sm: "122px", xs: "40px" },
+          mt: { sm: "100px", xs: "30px" },
+          justifyContent: "none",
+        }}
+      >
+        <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "#000000",
+              borderBottom: "3px solid #2c2821",
+              fontWeight: "bold",
+            }}
+          >
+            Home
+          </Link>
+          <a
+            href="#exercises"
+            style={{
+              textDecoration: "none",
+              color: "#000000",
+              fontWeight: "bold",
+            }}
+          >
+            Exercises
+          </a>
+        </Stack>
+      </Stack>
+    </header>
+  );
 };
 
 export default Navbar;
