d925fdb916e4451613a12aac7d61efea14613cd3
Noey <noey.rachata@gmail.com>
2023-05-29 09:16:20 +0700

    Remove unwanted MUI component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 // MUI components
-import { Box, createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 // hook
 import { useState } from "react";
@@ -10,31 +10,29 @@ import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 
 const Home = () => {
-  const [bodyPart, setBodyPart] = useState("all");
-  const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState("all");
+    const [exercises, setExercises] = useState([]);
 
-  const theme = createTheme({
-    typography: {
-      fontFamily: ["Playfair", "serif"].join(","),
-    },
-  });
-  return (
-    <ThemeProvider theme={theme}>
-      <Box>
-        <HeroBanner />
-        <SearchExercises
-          setExercises={setExercises}
-          bodyPart={bodyPart}
-          setBodyPart={setBodyPart}
-        />
-        <Exercises
-          setExercises={setExercises}
-          bodyPart={bodyPart}
-          exercises={exercises}
-        />
-      </Box>
-    </ThemeProvider>
-  );
+    const theme = createTheme({
+        typography: {
+            fontFamily: ["Playfair", "serif"].join(","),
+        },
+    });
+    return (
+        <ThemeProvider theme={theme}>
+            <HeroBanner />
+            <SearchExercises
+                setExercises={setExercises}
+                bodyPart={bodyPart}
+                setBodyPart={setBodyPart}
+            />
+            <Exercises
+                setExercises={setExercises}
+                bodyPart={bodyPart}
+                exercises={exercises}
+            />
+        </ThemeProvider>
+    );
 };
 
 export default Home;
