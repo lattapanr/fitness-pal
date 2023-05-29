62928a3ad222528104bbe1612d1e6e4e91a8e049
Noey <noey.rachata@gmail.com>
2023-05-29 09:17:15 +0700

    Chage width of page on large screen

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -9,17 +9,17 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const RootLayout = () => {
-  return (
-    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
-      <Navbar />
+    return (
+        <Box width="400px" sx={{ width: { xl: "1200px" } }} m="auto">
+            <Navbar />
 
-      <main>
-        <Outlet />
-      </main>
+            <main>
+                <Outlet />
+            </main>
 
-      <Footer />
-    </Box>
-  );
+            <Footer />
+        </Box>
+    );
 };
 
 export default RootLayout;
